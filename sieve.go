891e4d6777ced7521a2d78a4de3dc9e84cedba48
Heinrich Nirschl <heinrich.nirschl@gmail.com>
2019-03-17 10:36:26 +0100

    add movingSieve

--- sieve.go
+++ sieve.go
@@ -1,12 +1,25 @@
 package primes
 
+import (
+	"sync"
+
+	"github.com/ghhenry/intsqrt"
+)
+
 // baseSieve has one bit for each odd number below 2^16.
 // A set bit indicates the number is not prime.
 // From n to bit: n/2
 // From bit to n: 2*bit+1
 var baseSieve [512]uint64
+var initMutex sync.Mutex
 
 func initBase() {
+	initMutex.Lock()
+	defer initMutex.Unlock()
+	if baseSieve[0]&1 != 0 {
+		// already initialized
+		return
+	}
 	for bit := 1; bit < 1<<7; bit++ {
 		if baseSieve[bit/64]&(1<<uint(bit%64)) != 0 {
 			continue
@@ -16,4 +29,47 @@ func initBase() {
 			baseSieve[i/64] |= 1 << uint(i%64)
 		}
 	}
+	baseSieve[0] |= 1
+}
+
+// Iterate calls consumer with all primes between min and max (inclusive).
+func Iterate(min, max uint32, consumer func(p uint32) bool) {
+	initBase()
+	// 2 is a special case
+	if min <= 2 && 2 <= max && consumer(2) {
+		return
+	}
+	// primes from base sieve
+	if min < 1<<16 {
+		var bit uint32
+		bound := max / 2
+		if bound >= 1<<15 {
+			bound = 1<<15 - 1
+		}
+		for bit = min / 2; bit <= bound; bit++ {
+			if baseSieve[bit/64]&(1<<uint(bit%64)) == 0 && consumer(2*bit+1) {
+				return
+			}
+		}
+		min = 1 << 16
+	}
+	// primes after the base sieve
+}
+
+// initMoving initializes a moving sieve
+func initMoving(start uint32, sieve []uint64) {
+	end := uint32(len(sieve) * 64)
+	pmax := intsqrt.Sqrt32(2*(start+end) - 1)
+	Iterate(3, pmax, func(p uint32) bool {
+		s := p * p / 2
+		if s >= start {
+			s -= start
+		} else {
+			s = p - 1 - (start-s-1)%p
+		}
+		for i := s; i < end; i += p {
+			sieve[i/64] |= 1 << uint(i%64)
+		}
+		return false
+	})
 }
--- sieve_test.go
+++ sieve_test.go
@@ -9,7 +9,7 @@ import (
 // slow but safe primality test
 func isPrime(n uint32) bool {
 	if n&1 == 0 {
-		return false
+		return n == 2
 	}
 	lim := intsqrt.Sqrt32(n)
 	var i uint32
@@ -33,6 +33,63 @@ func TestInitBase(t *testing.T) {
 			if baseSieve[bit/64]&(1<<uint(bit%64)) == 0 {
 				t.Errorf("%v is composite but indicated as prime", n)
 			}
+		}
+	}
+}
+
+func TestInitMoving(t *testing.T) {
+	var tests = []struct {
+		start uint32
+	}{
+		{0},
+		{1},
+		{500},
+		{1 << 16},
+		{1 << 20},
+		{1 << 24},
+		{1 << 28},
+		{1<<31 - 128},
+	}
+	for _, test := range tests {
+		s := make([]uint64, 2)
+		initMoving(test.start, s)
+		for i := uint32(0); i < 128; i++ {
+			n := 2*(i+test.start) + 1
+			if isPrime(n) {
+				if s[i/64]&(1<<uint(i%64)) != 0 {
+					t.Errorf("%v is prime but indicated as composite", n)
+				}
+			} else {
+				if s[i/64]&(1<<uint(i%64)) == 0 {
+					t.Errorf("%v is composite but indicated as prime", n)
+				}
+			}
+		}
+	}
+}
+
+func TestIterateSmall(t *testing.T) {
+	var tests = []struct {
+		min, max, count uint32
+	}{
+		{0, 1000, 168},
+		{1001, 2000, 135},
+		{2001, 5000, 366},
+		{10000, 1 << 16, 5313},
+		{20000, 1 << 17, 9989},
+		{1<<32 - 500, 1<<32 - 1, 18},
+	}
+	for _, test := range tests {
+		count := uint32(0)
+		Iterate(test.min, test.max, func(p uint32) bool {
+			if !isPrime(p) {
+				t.Errorf("%v is composite but indicated as prime", p)
+			}
+			count++
+			return false
+		})
+		if count != test.count {
+			t.Errorf("found %v primes, but there should be %v", count, test.count)
 		}
 	}
 }
